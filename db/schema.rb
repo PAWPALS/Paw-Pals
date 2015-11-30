743eaeccbeabc655970a5c798871d6ac3ff8b6f6
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-30 14:36:56 -0500

    pet check in ans notice model; db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,62 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151130184753) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.string   "address",    null: false
+    t.string   "city",       null: false
+    t.string   "state",      null: false
+    t.integer  "zip",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pet_check_ins", force: :cascade do |t|
+    t.integer  "pet_id",     null: false
+    t.decimal  "longitude",  null: false
+    t.decimal  "latitude",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pet_notices", force: :cascade do |t|
+    t.integer  "pet_id",     null: false
+    t.decimal  "longitude",  null: false
+    t.decimal  "latitude",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.integer  "user_id",                    null: false
+    t.string   "name",                       null: false
+    t.string   "breed",                      null: false
+    t.integer  "age",                        null: false
+    t.string   "description",                null: false
+    t.boolean  "present",     default: true, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "access_token"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+end
