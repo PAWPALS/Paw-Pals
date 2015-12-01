12462dbd8bd3471c714a5411884676f618c25c83
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-30 20:04:09 -0500

    create, index, show, destroy methods pets controller

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,55 @@
+class PetsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy, :update]
+
+  def create
+    @pet = Pet.new(name: params[:name],
+                  breed: params[:breed],
+                  age: params[:age],
+                  description: params[:description],
+                  present: params[:present]
+                  user_id: current_user.id)
+                  }
+    if @pet.save
+      render "create.json.jbuilder", status: :created
+    else
+      render json: { errors: @pet.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @pets = Pet.all
+    render "index.json.jbuilder", status: :accepted 
+  end
+
+  def users_index
+  end
+
+
+  def show
+     @pet = Pet.find(params[:pet_id])
+    render "show.json.jbuilder", status: :accepted 
+  end
+
+  def destroy
+    @pet = Pet.find(params[:pet_id])
+     if @pet && current_user.id == @pet.user_id
+      @pet.destroy
+      render json: {success: "Pet delete successful!"}, status: :accepted 
+    else    
+     render json: { error: "Unable to delete the pet." },    
+            status: :unauthorized   
+    end
+  end
+
+  def update
+   @pet = Pet.find(params[:pet_id])
+    if @pet && current_user.id == @pet.user_id
+     @pet.update(present: params[:present])
+     render json: {success: "Pet status update successful!"}, status: :accepted
+    else
+      render json: { error: "Unable to edit pets status." },    
+            status: :unauthorized   
+    end
+  end
+end
