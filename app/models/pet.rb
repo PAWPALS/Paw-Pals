ccccd3b452f29ce1311be57b74f603c96a9a97e6
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-01 11:50:27 -0500

    added paperclip configs and validations to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,10 @@ class Pet < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :name, :breed, :age, :description, :present 
+
+  has_attached_file :avatar 
+  validates :avatar, avatar_presence: true
+  validates_attachment_file_name :gift, matches: [ /gif\Z/,
+    /jpg\Z/, /jpeg\Z/, /png\Z/, /jif\Z/, /jfif\Z/]
 end
 
