f3b9231e59d7923b8a8170bf0eb0586d0e1fd830
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-01 19:16:31 -0500

    added route see particular user address

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   resources :addresses do 
   end
 
+  get "/users/:id/addresses", to: "addresses#users_address"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
