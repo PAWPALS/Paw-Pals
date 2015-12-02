5ebbf1dc6942044424f2790dd9c1b118d3131b52
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-02 13:11:51 -0500

    added some validations

--- app/models/pet_check_in.rb
+++ app/models/pet_check_in.rb
@@ -1,3 +1,9 @@
 class PetNotice < ActiveRecord::Base
   belongs_to :pet
+
+  validates_presence_of :longitude, :latitude, :pet_id 
 end
+
+
+
+ 
--- app/models/pet_notice.rb
+++ app/models/pet_notice.rb
@@ -1,3 +1,5 @@
 class PetNotice < ActiveRecord::Base
   belongs_to :pet
+
+  validates_presence_of :longitude, :latitude, :pet_id
 end
