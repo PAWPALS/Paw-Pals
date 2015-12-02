5b42489a18d5acc03672fd1bb2c7950f4cdf426a
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-01 19:15:20 -0500

    finished addresses controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -20,7 +20,7 @@ class AddressesController < ApplicationController
   end
 
    def users_address
-    @address = Address.where(user_id: params[:id])
+    @address = Address.find_by!(user_id: params[:id])
     render "users_address.json.jbuilder", status: :accepted
   end
 
@@ -30,7 +30,7 @@ class AddressesController < ApplicationController
   end
 
   def destroy
-    @address = Address.find(params[:address_id])
+    @address = Address.find_by!(params[:address_id])
      if @address && current_user.id == @address.user_id
       @address.destroy
       render json: {success: "Address delete successful!"}, status: :accepted 
@@ -41,7 +41,7 @@ class AddressesController < ApplicationController
   end
 
   def update
-   @address = Address.find(params[:address_id])
+   @address = Address.find_by!(params[:address_id])
     if @address && current_user.id == @address.user_id
      @address.update(address: params[:street_address], city: params[:city], state: params[:state], zip: params[:zip])
      render json: {success: "Street address: #{@address.address}, City: #{@address.city},
