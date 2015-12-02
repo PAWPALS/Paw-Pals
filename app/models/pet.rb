464e6baf4eb370dac8eb8be97c9ee07090ca6bbd
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-02 11:29:08 -0500

    commented out avatar validation for now

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,11 @@
 class Pet < ActiveRecord::Base
   belongs_to :user
 
-  validates_presence_of :name, :breed, :age, :description, :present 
+  validates_presence_of :name, :breed, :age, :description
+
 
   has_attached_file :avatar 
-  validates :avatar, attachment_presence: true
+  # validates :avatar, attachment_presence: true
   validates_attachment_file_name :avatar, matches: [ /gif\Z/,
     /jpg\Z/, /jpeg\Z/, /png\Z/, /jif\Z/, /jfif\Z/]
 end
