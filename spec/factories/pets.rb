69fdd55b3b795b6803c6856eb3ad0809fb910c74
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-05 15:16:16 -0500

    pet factory and some examples

--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :pet do
-    
+    name "Tex"
+    breed "golden Retriever"
+    age "13"
+    description "cutest dog ever"
   end
-
 end
+
+
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Pet do
+    it 'has a valid factory' do 
+      expect(build(:pet)).to be_valid 
+  end
+
+   it "is invalid without without name" do
+    expect(build(:pet, name: nil)).to_not be_valid 
+  end
+
+  it "is invalid without without breed" do
+    expect(build(:pet, breed: nil)).to_not be_valid 
+  end
+
+  it "is invalid without without age and description" do
+    expect(build(:pet, age: nil, description: nil)).to_not be_valid 
+  end
+
+  it {should have_attached_file(:avatar)}
+  it {should validate_attachment_content_type(:avatar).
+     allowing(/gif\Z/,/jpg\Z/, /jpeg\Z/, /png\Z/, /jif\Z/, /jfif\Z/)}
+ end
 end
