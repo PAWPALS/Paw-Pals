d0fb5939e8b0593b3df82dcd494085457f4334ce
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-05 16:55:49 -0500

    address factory and examples

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Address, type: :model do
+
+  describe Address do
+    it "has a valid factory" do
+    expect(build(:address)).to be_valid 
+  end
+
+  it "is invalid without without address" do
+    expect(build(:address, address: nil)).to_not be_valid 
+  end
+
+  it "is invalid without without city" do
+    expect(build(:address, city: nil)).to_not be_valid 
+  end
+
+  it "is invalid without without state" do
+    expect(build(:address, state: nil)).to_not be_valid 
+  end
+
+  it "is invalid without without zip" do
+    expect(build(:address, zip: nil)).to_not be_valid 
+  end
+
+  it {should belong_to(:user)}
+ end
+end
+
