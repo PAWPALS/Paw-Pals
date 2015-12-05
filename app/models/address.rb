4f7ed9972bf2dcf6ceb4ac274a9662b5afd1988e
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-04 19:18:53 -0500

    added geocoder validations and methods

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,11 @@ class Address < ActiveRecord::Base
 
   validates_presence_of :address, :city, :state, :zip
   validates_uniqueness_of :address
+
+  geocoded_by :address
+  after_validation :geocode 
+
+  def address
+    [address, city, state, zip].compact.join(',')
+  end
 end
