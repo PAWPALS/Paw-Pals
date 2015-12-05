93d1f73de32aba1f829ea3bd6177bc1caf6257a8
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-05 14:11:31 -0500

    user factory and some examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe User do
+    it "has a valid factory" do
+    expect(build(:user)).to be_valid 
+  end
+
+  it "is invalid without without email" do
+    expect(build(:user, email: nil)).to_not be_valid 
+  end
+
+  it "is invalid without without password" do
+    expect(build(:user, password: nil)).to_not be_valid 
+  end
+ end
 end
+
+
+
+ 
+
+
+
+
