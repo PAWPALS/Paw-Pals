a655392d60dc48a938e5d7461e48d3d671132562
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-10 14:39:05 -0500

    added last_checkin_time to pets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210162647) do
+ActiveRecord::Schema.define(version: 20151210191950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,12 +29,12 @@ ActiveRecord::Schema.define(version: 20151210162647) do
   end
 
   create_table "pet_check_ins", force: :cascade do |t|
-    t.integer  "pet_id",      null: false
-    t.float    "longitude",   null: false
-    t.float    "latitude",    null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "location_id", null: false
+    t.integer  "pet_id",              null: false
+    t.float    "longitude",           null: false
+    t.float    "latitude",            null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.datetime "adafruit_created_at", null: false
   end
 
   create_table "pet_notices", force: :cascade do |t|
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20151210162647) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.datetime "last_checkin_time",                  null: false
   end
 
   create_table "users", force: :cascade do |t|
