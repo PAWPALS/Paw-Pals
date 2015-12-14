4605159890d90b914861f4681f47871208a0dc44
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-14 13:30:38 -0500

    try to fix heroku error

--- app/controllers/pet_checkins_controller.rb
+++ app/controllers/pet_checkins_controller.rb
@@ -2,7 +2,7 @@ class PetCheckinsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @pet = Pet.find_by!(params[:pet_id])
+    @pet = Pet.find(params[:id])
     if @pet.sync_from_api
       @pet_check_in = PetCheckIn.where(params[:pet_id]).last
        render "pet_checkin_show.json.jbuilder", status: :accepted
@@ -13,7 +13,7 @@ class PetCheckinsController < ApplicationController
   end
 
    def index
-    @pet = Pet.find_by!(params[:pet_id])
+    @pet = Pet.find(params[:id])
     if @pet.sync_from_api
       @pet_check_ins = PetCheckIn.where(params[:pet_id]).last(5)
     render "pet_checkin_index.json.jbuilder", status: :accepted 
