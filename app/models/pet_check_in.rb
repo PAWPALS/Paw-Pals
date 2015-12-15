190a649df255564eb5c164a6f4846ea27170d594
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-15 17:28:41 -0500

    added validations

--- app/models/pet_check_in.rb
+++ app/models/pet_check_in.rb
@@ -1,7 +1,8 @@
 class PetCheckIn < ActiveRecord::Base
   belongs_to :pet
 
-  validates_presence_of :longitude, :latitude, :pet_id 
+  validates_presence_of :longitude, :latitude, :pet_id, 
+  :adafruit_created_at, :adafruit_id 
 end
 
 
