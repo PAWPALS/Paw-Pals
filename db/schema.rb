dce7075288155b610fd1cb7af2bcd28f19a30bd3
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-15 17:26:27 -0500

    added mobile_url field to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215200042) do
+ActiveRecord::Schema.define(version: 20151215222250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20151215200042) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.datetime "last_checkin_time"
+    t.string   "mobile_url"
   end
 
   create_table "users", force: :cascade do |t|
