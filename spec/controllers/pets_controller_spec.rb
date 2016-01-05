ba6d55cc1e2934b6d0906b48daa73f8b3c827840
Ryan McWilliam <ryanmcwilliam@gmail.com>
2016-01-05 13:31:53 -0500

    refactored and added some new examples

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe PetsController, type: :controller do
 
-      
+    describe PetsController do
+      it {should use_before_action(:authenticate_user!)}
+    end
       
 
-        let(:user) { FactoryGirl.create(:user) }
-        before(:each) do
-        allow(controller).to receive(:current_user).and_return(user) 
-      end
+      let(:user) { FactoryGirl.create(:user) }
+      before(:each) do
+      allow(controller).to receive(:current_user).and_return(user) 
+    end
 
     describe "POST #create" do
 
-      it "allows logged in user to create pet in the database with valid attributes and sends status created" do
+    it "allows logged in user to create pet" do
       params = FactoryGirl.attributes_for(:pet) 
       post :create, params 
       expect(Pet.count).to eq(1)
@@ -20,7 +22,7 @@ RSpec.describe PetsController, type: :controller do
       expect(response).to render_template("pets/create.json.jbuilder")
     end
 
-    it "does not allow logged in user to create pet in the database with invalid attributes and sends status unprocessable" do
+    it "does not allow logged in user to create pet" do
       params = FactoryGirl.attributes_for(:pet, name: nil)
       post :create, params
       expect(Pet.count).to eq(0)
@@ -30,10 +32,35 @@ RSpec.describe PetsController, type: :controller do
 
     describe "GET #index" do 
 
-      it "it populates an array of pets for logged in user and sends status accepted" do
-        get :index 
-        expect(response).to have_http_status(202)
-        expect(response).to render_template("pets/index.json.jbuilder")
-      end
+    it "creates a resource" do
+      get :index 
+      expect(response).to have_http_status(202)
+      expect(response.content_type).to eq("application/json")
+      expect(response).to render_template("pets/index.json.jbuilder")
+    end
+  end
+
+  describe "GET #users_index" do
+
+    it "creates a resource" do
+      get :users_index, id: 8
+      expect(response).to have_http_status(202)
+      expect(response.content_type).to eq("application/json")
+      expect(response).to render_template("pets/users_index.json.jbuilder")
+    end
+  end
+
+  describe "GET #show" do
+
+    it "creates a resource" do
+    end
   end
 end
+
+
+
+
+
+
+
+
