b8d83b986034bc6ed4002c7fef1bdf7797f4ed9e
Ryan McWilliam <ryanmcwilliam@gmail.com>
2016-01-09 13:16:04 -0500

    added delayed_jobs table and set queue adapter

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221220444) do
+ActiveRecord::Schema.define(version: 20160109175557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,22 @@ ActiveRecord::Schema.define(version: 20151221220444) do
     t.float    "latitude",   null: false
     t.float    "longitude",  null: false
   end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "pet_check_ins", force: :cascade do |t|
     t.integer  "pet_id",              null: false
